e5d20331054087fcc53059e6e491e9d6fa76bbab
ChrisLangsford <langz020@gmail.com>
2014-10-09 10:41:01 +0200

    added booklet

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,14 @@
 class WelcomeController < ApplicationController
   def index
   	send_file("#{Rails.root}/public/Slide.pdf",
-              filename: "Slide.pdf",
+              filename: "Project Day Brochure 2014.pdf",
               type: "application/pdf")
 
   end
 
 	def download_pdf
 		send_file("#{Rails.root}/public/Slide.pdf",
-              filename: "Slide.pdf",
+              filename: "Project Day Brochure 2014.pdf",
               type: "application/pdf")
 		
 	end
